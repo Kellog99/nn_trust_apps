a3713303abdf60613841645d3e3748e2c7eb3db4
cristianoCarta <cristiano.carta23@gmail.com>
2025-10-01 11:48:57 +0200

    attack router stabs

---
+++ attack-server/routers/attack_router.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter 
+from typing import List
+from annotated_types import Gt, Ge, Le, Lt
+from nn_trust.attack._evasion import EvasionAttackFactory as EAF
+from nn_trust.attack.evaluation._statistics import StatisticsFactory as SF
+from nn_trust.core import Task
+from pydantic_core import PydanticUndefined
+from models import AttackProps, ParametersProps, MetricProps
+
+# ---------------- Utilities --------------- #
+
+"""
+import here every function that you need in the services
+"""
+
+#from lib.attack_utils import ecc...
+
+# ---------------- Services ---------------- #
+
+router = APIRouter(prefix="/attack", tags=["attacks and statistics"])
