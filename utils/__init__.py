194526f502da848ac6894c0cb62f214a83153eeb
Andrea Chelini <a.chelini99@gmail.com>
2026-09-24 17:14:43 +0200

    fixing circular imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
-from utils.load_model import load_model, clear_model_cache
+from utils.load_model import load_model
+from utils.model._loader_nlp_models import clear_model_cache
 from utils.load_dataset import get_dataloader
